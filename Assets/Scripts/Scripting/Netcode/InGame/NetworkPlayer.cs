dcc67fb9f0f51102bee754016687b43e83a0f110
Ryan A <ryanabusa8134@gmail.com>
2022-02-23 17:49:08 -0500

    Added Network Player Controller Fucntion
    Make sure to infer on the player controller xr rig video, some things I did aren't a hundered on if they are right or not...

---
+++ Assets/Scripts/Scripting/Netcode/InGame/NetworkPlayer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SpatialTracking;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private Vector2 placementArea = new Vector2(-10.0f, 10.0f);
+
+    public override void OnNetworkSpawn()
+    {
+        DisableClientInput();
+    }
+
+    public void DisableClientInput()
+    {
+        if (IsClient && IsOwner)
+        {
+            var clientMoveProvider = GetComponent<NetworkMoveProvider>();
+            var clientHead = GetComponentInChildren<TrackedPoseDriver>();
+            var clientCamera = GetComponentInChildren<Camera>();
+            var clientControllers = GetComponentInChildren<XRController>();
+
+            clientCamera.enabled = false;
+            //clientMoveProvider.EnableInputActions = false;
+            clientHead.enabled = false;
+
+            
+        }
+    }
+
+    private void Start()
+    {
+        if (IsClient && IsOwner)
+        {
+            transform.position = new Vector3(Random.RandomRange(placementArea.x, placementArea.y),
+                transform.position.y, Random.RandomRange(placementArea.x, placementArea.y));
+        }
+    }
+}
