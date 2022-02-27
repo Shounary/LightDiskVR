45d5fff3b56734734c94f0b5fde8a8e019bc20eb
ChaseOB <chaseobrien128@gmail.com>
2022-02-26 23:42:09 -0500

    added weapon script

---
+++ Assets/Scripts/Scripting/Weapons/Weapon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Color primaryColor;
+    public Color accentColor;
+    public float damage;
+    public bool isSummonable;
+    public List<string> storableLocations;
+}
