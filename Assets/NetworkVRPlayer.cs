b3db1b511d9f664107882b053382abafdfd8fcdb
mromanenko3 <mromanenko3@gatech.edu>
2022-03-02 17:21:20 -0500

    7

--- Assets/NetworkVRPlayer.cs
+++ Assets/NetworkVRPlayer.cs
@@ -10,47 +10,55 @@ public class NetworkVRPlayer : NetworkBehaviour
     [SerializeField]
     private Vector2 placementArea = new Vector2(-10.0f, 10.0f);
 
+
     public void EnableClientInput()
     {
         if (IsClient && IsOwner)
         {
-            var clientControllers = GetComponentsInChildren<ActionBasedController>();
-            foreach (var controller in clientControllers)
-            {
-                controller.enableInputActions = true;
-                controller.enableInputTracking = true;
-            }
+            // var clientControllers = GetComponentsInChildren<ActionBasedController>();
+            // foreach (var controller in clientControllers)
+            // {
+            //     controller.enableInputActions = true;
+            //     controller.enableInputTracking = true;
+            // }
 
-            var clientCamera = GetComponentInChildren<Camera>();
-            clientCamera.enabled = true;
+            // var clientCamera = GetComponentInChildren<Camera>();
+            // clientCamera.enabled = true;
 
-            var clientHead = clientCamera.gameObject.AddComponent<TrackedPoseDriver>();
-            clientHead.enabled = true;
+            //var clientHead = clientCamera.gameObject.AddComponent<TrackedPoseDriver>();
+            //clientHead.enabled = true;
 
         }
     }
 
     public void DisableClientInput()
     {
-        var clientControllers = GetComponentsInChildren<ActionBasedController>();
-        var clientHead = GetComponentInChildren<TrackedPoseDriver>();
-        var clientCamera = GetComponentInChildren<Camera>();
+        if (!IsOwner) {
+            var clientControllers = GetComponentsInChildren<ActionBasedController>();
+            var clientHead = GetComponentInChildren<TrackedPoseDriver>();
+            var clientCamera = GetComponentInChildren<Camera>();
 
-        clientCamera.enabled = false;
-        clientHead.enabled = false;
+            clientCamera.enabled = false;
+            clientHead.enabled = false;
 
-        foreach (var controller in clientControllers)
-        {
-            controller.enableInputActions = false;
-            controller.enableInputTracking = false;
+            foreach (var controller in clientControllers)
+            {
+                controller.enableInputActions = false;
+                controller.enableInputTracking = false;
+            }
+
         }
     }
 
     private void Start()
     {
+        DisableClientInput();
         transform.position = new Vector3(Random.RandomRange(placementArea.x, placementArea.y),
                 transform.position.y, Random.RandomRange(placementArea.x, placementArea.y));
     }
 
+    public void OnNetworkSpawn() {
+        DisableClientInput();
+    }
     //public void OnSelectGrabbable(Se)
 }
