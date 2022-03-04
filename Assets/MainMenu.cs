f838d1cbda1242d14039c3c54dbfd7ed3e033172
ChaseOB <chaseobrien128@gmail.com>
2022-03-04 04:23:54 -0500

    start menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;
+    public Button settingsButton;
+    public Button quitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
