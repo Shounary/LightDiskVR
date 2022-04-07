c9346b852d4c780309a4831fff652dae1bf5263b
mromanenko3 <mromanenko3@gatech.edu>
2022-04-07 00:19:31 -0400

    Revert "velocity scale"
    This reverts commit 3d139d6bd7fe0d0ad7dddc62fe9a128954342645.

--- Assets/Scripts/Scripting/Weapons/Weapon.cs
+++ Assets/Scripts/Scripting/Weapons/Weapon.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
-using UnityEngine.XR.Interaction.Toolkit;
+
 
 
 public class Weapon : MonoBehaviour
@@ -53,7 +53,6 @@ public class Weapon : MonoBehaviour
         p_prev = weaponRB.position;
         r_prev = weaponRB.rotation;
 
-        interactable = GetComponent<XRGrabInteractable>();
     }
 
     public virtual void TriggerFunction(float additionalFactor, Transform targetTransform)
@@ -160,14 +159,8 @@ public class Weapon : MonoBehaviour
     public Vector3 ComputedVelocity { get; private set; }
     public Vector3 ComputedAngularVelocity { get; private set; }
 
-    public Vector3 ComputedReleaseVelocity => ComputedVelocity * interactable.throwVelocityScale;
-    public Vector3 ComputedReleaseAngularVelocity => ComputedAngularVelocity * interactable.throwAngularVelocityScale;
-
     Vector3 p_prev;
     Quaternion r_prev;
-
-    XRGrabInteractable interactable;
-
     private void FixedUpdate()
     {
         var p = weaponRB.position;
