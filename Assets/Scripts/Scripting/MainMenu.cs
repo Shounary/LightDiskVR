8da32ad7af345a2439714d5e596662a3d885e042
ChaseOB <chaseobrien128@gmail.com>
2022-04-15 12:57:39 -0400

    main menu mostly done

--- Assets/Scripts/Scripting/MainMenu.cs
+++ Assets/Scripts/Scripting/MainMenu.cs
@@ -45,7 +45,7 @@ public class MainMenu : MonoBehaviour
     public void onButton2Click() {
         switch(currActiveMenu) {
             case 0:
-                openSubMenu(4);
+                //openSubMenu(4);
                 break;
             case 1:
                 openSubMenu(3);
@@ -72,7 +72,7 @@ public class MainMenu : MonoBehaviour
                 goToScene("Adv Tutorial");
                 break;
             case 3:
-                goToScene("ShootingRange");
+                goToScene("ShootingRange70sec");
                 break;
             default:
                 break;
